3ece9a218fd56f4d57fbb2410c21136d1ed235e0
iKuchko <illiak@dataabstractsolutions.com>
2018-11-26 16:45:06 -0800

    DBResultParser return LocalDate and LocalDateTime

--- src/main/java/com/illiakins/DBResultParser.java
+++ src/main/java/com/illiakins/DBResultParser.java
@@ -1,6 +1,9 @@
 package com.illiakins;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +40,38 @@ public class DBResultParser {
         }
         if (resultRow.get(key) == null || resultRow.get(key).equals("")) return null;
         return (Boolean.valueOf(resultRow.get(key).toString()) || resultRow.get(key).toString().equals("1"));
+    }
+
+    public LocalDate getLocalDate(String key) throws SQLException {
+        if (!resultRow.containsKey(key)) {
+            throw new SQLException("ResultSet does not contain column with name '" + key +"'");
+        }
+        if (resultRow.get(key) == null || resultRow.get(key).equals("")) return null;
+        return LocalDate.parse(resultRow.get(key).toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public LocalDate getLocalDate(String key, DateTimeFormatter dateTimeFormatter) throws SQLException {
+        if (!resultRow.containsKey(key)) {
+            throw new SQLException("ResultSet does not contain column with name '" + key +"'");
+        }
+        if (resultRow.get(key) == null || resultRow.get(key).equals("")) return null;
+        return LocalDate.parse(resultRow.get(key).toString(), dateTimeFormatter);
+    }
+
+    public LocalDateTime getLocalDateTime(String key) throws SQLException {
+        if (!resultRow.containsKey(key)) {
+            throw new SQLException("ResultSet does not contain column with name '" + key +"'");
+        }
+        if (resultRow.get(key) == null || resultRow.get(key).equals("")) return null;
+        return LocalDateTime.parse(resultRow.get(key).toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S"));
+    }
+
+    public LocalDateTime getLocalDateTime(String key, DateTimeFormatter dateTimeFormatter) throws SQLException {
+        if (!resultRow.containsKey(key)) {
+            throw new SQLException("ResultSet does not contain column with name '" + key +"'");
+        }
+        if (resultRow.get(key) == null || resultRow.get(key).equals("")) return null;
+        return LocalDateTime.parse(resultRow.get(key).toString(), dateTimeFormatter);
     }
 
     public HashMap<String, Object> getResultHashMap() {
